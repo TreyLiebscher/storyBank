6bf3819f4bcbfae909cd9fc83f90ddf303a170f7
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-07-17 16:04:38 +0300

    refactored some constants into globals.js

--- public/app.js
+++ public/app.js
@@ -1,11 +1,5 @@
 'use strict';
 
-const BASE_URL = '/'
-const API_URLS = {
-    createBlock: '/storyblock/block/create',
-    createStory: '/stories/story/create' //NOTE: need to append  block id
-}
-
 
 // http://localhost:8080/
 
