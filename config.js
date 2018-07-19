594d0c6115956de67a52a748a29f98c57f05ba1b
Trey Liebscher <wcliebsc@gmail.com>
2018-07-19 07:45:04 -0400

    removed unnecessary comment

--- config.js
+++ config.js
@@ -4,5 +4,3 @@ require('dotenv').config()
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/storybank2';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/storybank2';
 exports.PORT = process.env.PORT || 8080;
-
-// 'mongodb://localhost/storyBank'
