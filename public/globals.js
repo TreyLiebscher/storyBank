0b24a0b52ce8fbc3a481665785977ab5c81a7f85
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-07-19 20:48:34 +0300

    fixed client route

--- public/globals.js
+++ public/globals.js
@@ -2,5 +2,5 @@ const API_URLS = {
     createBlock: '/storyblock/block/create',
     getBlocks: '/storyblock/blocks',
     createStory: '/stories/story/create', //NOTE: need to append  block id
-    getBlocksWithStories: 'storyblock/blocks/stories'
+    getBlocksWithStories: '/storyblock/blocks/stories'
 }
