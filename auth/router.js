4eff862430c47a4a6fe137d031ba23b23d76f3f0
Trey Liebscher <wcliebsc@gmail.com>
2018-07-23 11:09:05 -0400

    added authentication router/strategies/index

---
+++ auth/router.js
@@ -0,0 +1,60 @@
+'use strict';
+const express = require('express');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const {UserModel} = require('../users/userModel');
+const config = require('../config');
+const router = express.Router();
+
+const createAuthToken = function(user) {
+    return jwt.sign({user}, config.JWT_SECRET, {
+        subject: user.email,
+        expiresIn: config.JWT_EXPIRY,
+        algorithm: 'HS256'
+    });
+};
+
+const localAuth = passport.authenticate('local', {session: false, failWithError: false});
+
+router.use(bodyParser.json());
+
+router.post('/login', localAuth, (req, res) => {
+    console.log('Login attempt successful', req.email, req.password, req.user)
+
+    const authToken = createAuthToken(req.user.serialize());
+    const email = req.user.serialize().email;
+    res.json({authToken, emailAddress});
+});
+
+const jwtAuth = passport.authenticate('jwt', {session: false});
+
+
+async function changePassword(req, res) {
+    const requiredFields = ['newPassword', 'retypeNewPassword'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`;
+            console.error(message);
+            return res.status(4044).send(message);
+        }
+    }
+
+    const userPassword = await UserModel.hashPassword(req.body.password);
+
+    const userRecord = await UserModel.findByIdAndUpdate(req.user.id, {password:userPassword});
+
+    res.json({
+        user: userRecord.serialize()
+    })
+}
+
+router.post('/changepassword', jwtAuth, tryCatch(changePassword));
+
+router.post('/refresh-auth-token', jwtAuth, (req, res) => {
+    const authToken = createAuthToken(req.user);
+    res.json({authToken});
+});
+
+module.exports = router;
---
+++ auth/strategies.js
@@ -0,0 +1,57 @@
+'use strict';
+const {Strategy: LocalStrategy} = require('passport-local');
+
+const {Strategy: JwtStrategy, ExtractJwt} = require('passport-jwt');
+
+const {UserModel} = require('../users/userModel');
+const {JWT_SECRET} = require('../config');
+
+const localStrategy = new LocalStrategy(
+    {
+        usernameField: 'email',
+        passwordField: 'password'
+    },
+    (email, password, callback) => {
+        console.log('LOCAL AUTH', email, password)
+        let user;
+        UserModel.findOne({email: email})
+            .then(_user => {
+                user = _user;
+                if (!user) {
+                    return Promise.reject({
+                        reason: 'LoginError',
+                        message: 'Incorrect email or password'
+                    });
+                }
+                return user.validatePassword(password);
+            })
+            .then(isValid => {
+                if (!isValid) {
+                    return Promise.reject({
+                        reason: 'LoginError',
+                        message: 'Incorrect email or password'
+                    });
+                }
+                return callback(null, user);
+            })
+            .catch(err => {
+                if (err.reason === 'LoginError') {
+                    return callback(null, false, err);
+                }
+                return callback(err, false);
+            });
+    }
+);
+
+const jwtStrategy = new JwtStrategy(
+    {
+        secretOrKey: JWT_SECRET,
+        jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+        algorithms: ['HS256']
+    },
+    (payload, done) => {
+        done(null, payload.user);
+    }
+);
+
+module.exports = {localStrategy, jwtStrategy};
