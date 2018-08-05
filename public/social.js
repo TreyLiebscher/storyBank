6978875ff60adf7b402a18b3c555736b04bd4fac
treyliebscher <wcliebsc@gmail.com>
2018-08-05 12:12:33 -0400

    created distinct section for discovered public stories

--- public/social.js
+++ public/social.js
@@ -17,9 +17,22 @@ function getRandomPublicStories() {
     })
 }
 
+function renderStoryPublicQuickView(result) {
+
+    return `
+    <div type="button" class="storyQuickView" style="background: linear-gradient( rgba(0, 0, 0, 0.9),
+    rgba(0, 0, 0, 0.6) ), url(${result.image});
+    background-repeat: no-repeat;
+    background-position: center;">
+    <h3 class="quickViewTitle">${result.title}</h3>
+    <p class="storyId">${ result.id }</p>
+    </div>
+    `
+}
+
 function displayPublicStories(arr) {
-    const results = arr.stories.map((item) => renderStoryQuickView(item));
-    $('.storyBlockView').html(results);
+    const results = arr.stories.map((item) => renderStoryPublicQuickView(item));
+    $('.discoverView').html(results);
     componentHandler.upgradeDom();
 }
 
@@ -29,5 +42,24 @@ function handleGetALlPublicStories() {
         getRandomPublicStories();
     });
 }
+
+function handleViewPublicStory() {
+    $('.discoverView').on('click', 'div.storyQuickView', function (event) {
+        event.preventDefault();
+        const storyId = $(event.target).closest('.storyQuickView').find('.storyId').text();
+        console.log('The story id is:', storyId);
+        const resultPromise = getStoryById(storyId);
+        $('.storyBlockView').empty();
+        resultPromise.catch(err => {
+            console.error('Error', err);
+        })
+
+        resultPromise.then(resultResponse => {
+            return displayStory(resultResponse);
+        })
+    })
+}
+
+$(handleViewPublicStory);
 
 $(handleGetALlPublicStories);
